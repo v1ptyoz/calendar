f7d1b2c710122577ad0eab650b7919da1f451a14
Alexander Khrabrov <v1ptyoz@gmail.com>
2020-04-30 14:22:43 +1000

    gitignore, monthConvert function

---
+++ script.js
@@ -0,0 +1,29 @@
+function monthConvert(month) {
+  switch (month) {
+    case 1: 
+      return "Январь";
+    case 2: 
+      return "Февраль";
+    case 3: 
+      return "Март";
+    case 4: 
+      return "Апрель";
+    case 5: 
+      return "Май";
+    case 6: 
+      return "Июнь";
+    case 7: 
+      return "Июль";
+    case 8: 
+      return "Август";
+    case 9: 
+      return "Сентябрь";
+    case 10: 
+      return "Октябрь";
+    case 11: 
+      return "Ноябрь";
+    case 12: 
+      return "Декабрь";
+  }
+}
+
